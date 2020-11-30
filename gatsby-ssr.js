cf6ce05a04683045973adb13bfb2411aeca8019f
Billy Peralta <37436984+billyperalta89@users.noreply.github.com>
2020-11-29 20:10:10 -0800

    Update gatsby-ssr.js
    Remove

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,14 +1,14 @@
-import React from "react"
-export const onRenderBody = (
-  { setHeadComponents }
-) => {
-  setHeadComponents([
-    <script
-			key="tracking"
-    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-      data-ad-client="ca-pub-5060970476414755"
-      async
-    />,
-  ])
+//import React from "react"
+//export const onRenderBody = (
+//  { setHeadComponents }
+//) => {
+//  setHeadComponents([
+//    <script
+//			key="tracking"
+//    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+//      data-ad-client="ca-pub-5060970476414755"
+//      async
+//    />,
+//  ])
   
-}
+//}
