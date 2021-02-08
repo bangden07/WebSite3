e96dca8109fc48b3fa0445800a660542d7c79d3e
Billy Peralta <billy@itfo.ca>
2021-02-07 17:23:48 -0800

    Test

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import React from "react"
 // export const onRenderBody = (
 //   { setHeadComponents }
@@ -13,19 +12,3 @@
 //   ])
 
 // }
-=======
-//import React from "react"
-//export const onRenderBody = (
-//  { setHeadComponents }
-//) => {
-//  setHeadComponents([
-//    <script
-//			key="tracking"
-//    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-//      data-ad-client="ca-pub-5060970476414755"
-//      async
-//    />,
-//  ])
-  
-//}
->>>>>>> cf6ce05a04683045973adb13bfb2411aeca8019f
